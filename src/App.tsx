56fe277d370ccc8b8db3baa5cb010193a4cb1a1e
ariis11 <aristidaslukas@gmail.com>
2024-06-04 22:25:48 +0300

    GP test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import EmailMarketer from './components/EmailMarketer';
 import Navbar from './components/NavBar';
