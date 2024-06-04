49b8d3c47fc4ef014825faca1fd57bb2c8f7c5f1
ariis11 <aristidaslukas@gmail.com>
2024-06-04 22:06:34 +0300

    Github Pages test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react';
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Home from './components/Home';
-// import EmailMarketer from './components/EmailMarketer';
+import { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/Home';
+import EmailMarketer from './components/EmailMarketer';
 
 export default function App() {
   useEffect(() => {
@@ -10,30 +10,15 @@ export default function App() {
     }
   }, []);
 
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
-        </a>
-        <a href="https://react.dev" target="_blank">
-          {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    Labas
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/email-marketer" element={<EmailMarketer />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
