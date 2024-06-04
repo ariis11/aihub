161877c4589cf3b132c78276a7114e869be632e7
ariis11 <aristidaslukas@gmail.com>
2024-06-04 22:35:09 +0300

    GP test

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom';
 export default function Home() {
   return (
     <div>
-      <div>
+      {/* <div>
         <Link to="/email-marketer">Email Marketer</Link>
         <Link to="/email-marketer">WEB3</Link>
         <Link to="/email-marketer">Crypto</Link>
         <Link to="/email-marketer">Other</Link>
         <Link to="/email-marketer">Other</Link>
-      </div>
+      </div> */}
+      LABAS
     </div>
   );
 };
