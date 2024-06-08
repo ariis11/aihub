76ca6b6fdafd3c2b497920582acc39bbdef8014b
ariis11 <aristidaslukas@gmail.com>
2024-06-08 21:42:58 +0200

    unused code

--- src/components/TechnicalAnalysis.tsx
+++ src/components/TechnicalAnalysis.tsx
@@ -20,17 +20,10 @@ export default function TechnicalAnalysis() {
         fetchNews();
     }, []);
 
-    const initialData = [
-        { time: Date.parse('2024-06-07 09:06') / 1000, value: 32.51 },
-        { time: Date.parse('2024-06-07 09:11') / 1000, value: 42.51 },
-        { time: Date.parse('2024-06-07 09:15') / 1000, value: 40.51 },
-    ];
-
     return (
         <div>
             <h1>Technical Analysis</h1>
             {coinData && <Chart data={coinData} />}
-            {/* <Chart data={initialData} /> */}
         </div>
     );
 }
