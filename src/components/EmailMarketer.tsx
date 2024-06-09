c9871ccb6fdd04d1d45b80f0133fe6541290e641
ariis11 <aristidaslukas@gmail.com>
2024-06-09 03:46:22 +0200

    GP build failed

--- src/components/EmailMarketer.tsx
+++ src/components/EmailMarketer.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 import { generateEmail, sendEmail } from "../http";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 export default function EmailMarketer() {
     const [emailContent, setEmailContent] = useState('');
