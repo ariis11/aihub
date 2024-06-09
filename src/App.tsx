9bcaeab3f4184d137beb1cc2854f40cc9cab074b
ariis11 <aristidaslukas@gmail.com>
2024-06-09 04:46:39 +0200

    ton connect

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import Home from './components/Home';
+import { TonConnectButton } from '@tonconnect/ui-react';
 
 export default function App() {
   useEffect(() => {
@@ -10,6 +11,7 @@ export default function App() {
 
   return (
     <>
+      <TonConnectButton />
       <Home />
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import App from './App.tsx'
 import './index.css'
 import WebApp from '@twa-dev/sdk'
 import { HashRouter as Router } from "react-router-dom";
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 WebApp.ready();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Router>
-    <App />
-  </Router>,
+  <TonConnectUIProvider manifestUrl='https://ariis11.github.io/aihub/tonconnect-manifest.json'>
+    <Router>
+      <App />
+    </Router>
+  </TonConnectUIProvider>,
 )
